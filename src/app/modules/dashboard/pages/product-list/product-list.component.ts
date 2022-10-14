f55bbddf26cc582470875f80cc27801db945efae
ronanborja <ronanborja1205@gmail.com>
2022-10-14 13:00:51 +0800

    Co-authored-by: camsxcz <camsxcz@users.noreply.github.com> Co-authored-by: jhondanielcatalan <jhondanielcatalan@users.noreply.github.com> Co-authored-by: Mark Nelson <MarkNels0n@users.noreply.github.com>

--- src/app/modules/dashboard/pages/product-list/product-list.component.ts
+++ src/app/modules/dashboard/pages/product-list/product-list.component.ts
@@ -1,145 +1,4 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Product } from '../../models/product';
-// import { ProductService } from '../../services/product.service';
-// import { CartService } from '../../services/cart.service';
-// import { Options,LabelType } from '@angular-slider/ngx-slider';
-
-
-// @Component({
-//   selector: 'app-product-list',
-//   templateUrl: './product-list.component.html',
-//   styleUrls: ['./product-list.component.scss'],
-// })
-// export class ProductListComponent implements OnInit {
-//   searchItem:string="";
-//   products: Product[] = [];
-//   categories:string[]=[];
-//   topProducts:Product[]=[];
-//   minValue: number = 0;
-//   maxValue: number = 1000000;
-//   public show:boolean = false;
-//   public buttonName:any = 'Show';
-//   filterCategory:string="";
-//   filterSequence:number=0;
-//   filterOrderName:string="";
-//   prevSort:string="";
-//   sortValue:string="";
-  
-
-//   constructor(private productService: ProductService,private cartService: CartService) {}
-
-//   options: Options = {
-//     floor: 1,
-//     ceil: 1000000,
-//     showTicks:true,
-//     stepsArray: [
-//       { value: 1},
-//       { value: 100},
-//       { value: 500},
-//       { value: 1000},
-//       { value: 10000},
-//       { value: 100000},
-//       { value: 1000000}  ],
-//     translate: (value: number, label: LabelType): string => {
-//       switch (label) {
-//         case LabelType.Low:
-//           return "<b>₱" + value+"</b>";
-//         case LabelType.High:
-//           return "<b>₱" + value+"</b>";
-//         default:
-//           return "";
-//       }
-//     }
-//   };
-
-//   findName(search:any): any
-//   {
-//     return 
-//   }
-
-  
-
-
-//   toggle() {
-//     this.show = !this.show;
-//     if(this.show) this.buttonName = "Hide";
-//     else  this.buttonName = "Show";
-//   }
-
-// inverse:boolean=false;
-//   ngOnInit(): void {
-//     this.productService.getProducts().subscribe((res) => {
-//       this.products = res;
-//       this.categories = res.map( (a:any) => String(a.productCategory));
-//       this.categories = this.categories.filter((value,index)=>this.categories.indexOf(value)===index);
-//       res.forEach((a: any) => {
-//         Object.assign(a, { quantity: 1, total: Number(a.productPrice)});
-//       });
-//     });
-
-//     this.productService.getTopProducts().subscribe((res =>
-//       {
-//         this.topProducts = res;
-//       }))
-
-//     this.cartService.search.subscribe(val=>
-//       {
-//         this.searchItem = val;
-//       })
-//     }
-  
-//   doSort(orderValue:string)
-//   {
-//     if(this.filterOrderName==orderValue)
-//     { 
-//       if(this.filterSequence==2)
-//       this.filterSequence=0;
-//       else
-//       {
-//         this.filterSequence++;
-//       }
-//     }
-//     else 
-//     {
-//       this.filterOrderName = orderValue;
-//       this.filterSequence = 0;
-//     }
-//     this.filter(this.filterCategory,this.filterOrderName,this.minValue,this.maxValue,this.filterSequence);
-//     console.log(orderValue);
-//   }
-
-//   categoryFilterChange(category:string)
-//   {
-//     this.filterCategory=category;
-//     this.filter(this.filterCategory,this.filterOrderName,this.minValue,this.maxValue,this.filterSequence);
-//   }
-//   priceFilterChange(event:any) {
-//     this.filter(this.filterCategory,this.filterOrderName,this.minValue,this.maxValue,this.filterSequence);
-//   }
-  
-//   filter(category:string,orderBy:string,min:number,max:number,filterSequence:number)
-//   {
-//      return this.productService.getProductsFilter(orderBy,category,min,max,filterSequence).subscribe((res) => {
-//        this.products = res;
-//      });
-// }
-
-//   addToCart(prod: Product) {
-//     this.cartService.addtoCart(prod);
-//   }
-
-//   isUser() {
-//     const role = localStorage.getItem("userType");
-//     if(role == "admin"){
-//       return false;
-//     }
-//      return true;
-//   }
-  
-// }
-
-
-import { Component, Inject, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
 import { CartService } from '../../services/cart.service';
@@ -161,12 +20,9 @@ export class ProductListComponent implements OnInit {
   public buttonName:any = 'Show';
   filterCategory:string="";
   sortOrder:number=0;
-  
-
-
-  constructor(@Inject(DOCUMENT) document: Document,private productService: ProductService,private cartService: CartService) {
-
-    
+  styleString="";
+  left=0;
+  constructor(private productService: ProductService,private cartService: CartService) { 
   }
 
   options: Options = {
@@ -193,11 +49,9 @@ export class ProductListComponent implements OnInit {
     }
   };
 
-  findName(search:any): any
-  {
-    return 
-  }
-
+ 
+ 
+  
 
   toggle() {
     this.show = !this.show;
@@ -205,19 +59,6 @@ export class ProductListComponent implements OnInit {
     else  this.buttonName = "Show";
   }
   ngOnInit(): void {
-    const thisDiv = document.getElementById('center');
-    if(thisDiv != null)
-    {
-    if(!this.isUser())
-    {
-    thisDiv.style.transform = `translate(${10}%,${0}%)`;
-    }
-    else
-    {
-    thisDiv.style.transform = `translate(${0}%,${0}%)`;
-    }
-    }
-
     this.productService.getProducts().subscribe((res) => {
       this.products = res;
       this.categories = res.map( (a:any) => String(a.productCategory));
@@ -226,6 +67,15 @@ export class ProductListComponent implements OnInit {
         Object.assign(a, { quantity: 1, total: Number(a.productPrice)});
       });
     });
+
+    if(!this.isUser())
+    {
+      this.left = 10;
+    }
+    else
+    {
+      this.left = -5;
+    }
 
     this.cartService.search.subscribe(val=>
       {
